4b4250c95650f0c0d6c8dfc0249e1f5a1d8e7493
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-02-27 18:32:30 -0300

    add nodemailer and types/nodemailer

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
     async execute(req: Request, res: Response) {
@@ -20,9 +21,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+        const survey = await surveysRepository.findOne({ id: survey_id });
 
-        if(!surveyAlreadyExists) {
+        if(!survey) {
             return res.status(400).json({
                 error: 'Survey does not exists!'
             }); 
@@ -34,6 +35,8 @@ class SendMailController {
         });
 
         await surveysUsersRepository.save(surveyUser);
+
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return res.json(surveyUser);
     }
