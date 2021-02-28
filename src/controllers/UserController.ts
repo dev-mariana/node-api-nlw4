f4228072e85405e85260bccc11b55d4ca0b9bb87
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-02-28 17:23:52 -0300

    add class AppError, custom errors and middlewares

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getCustomRepository, getRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
     async create(req: Request, res: Response) {
@@ -21,7 +22,7 @@ class UserController {
         try {
            await schema.validate(req.body, { abortEarly: false });
         } catch (error) {
-            return res.status(404).json({ error: error });
+            throw new AppError(error);
         }
 
         const usersRepository = getCustomRepository(UsersRepository);
@@ -31,9 +32,7 @@ class UserController {
         });
 
         if(userAlreadyExists) {
-            return res.status(400).json({
-                error: "User already exists"
-            });
+            throw new AppError('User already exists!');
         }
 
         const user = usersRepository.create({
